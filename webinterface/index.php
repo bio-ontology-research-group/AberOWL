6b5a48b2089c8616b92f7eeab19d5b0ed4dc8db0
leechuck <leechuck@leechuck.de>
2014-06-03 10:39:32 +0100

    updated php SPARQL plugin to work with VALUES and FILTER, use new webservices

--- webinterface/index.php
+++ webinterface/index.php
@@ -1,14 +1,56 @@
+<?php
+$query = $_GET["query"];
+$ontology = $_GET["ontology"];
+$type = $_GET["type"];
+if ($type == null) { 
+  $type = "subclass" ;
+}
+
+// OWL TYPE <endpoint> <ontURI> { query }
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
     <title>SPARQOWL Query Builder</title>
+        <script src="js/jquery-1.10.2.js"></script>  
+        <script src="js/jquery.autosize.js"></script>  
+<script language="javascript">
+  function change( type ) {
+    var ta = document.getElementById("ta");
+    if (type == 'values') {
+      ta.value = "SELECT * WHERE { \n" +
+    "# binds ?ontid to the results of the OWL query \n" +
+    "VALUES ?ontid { \n" +
+    "OWL <?php echo $type; ?> <http://jagannath.pdn.cam.ac.uk/aber-owl/service/> <<?php echo $ontology; ?>> { <?php echo $query; ?> }\n" +
+   "} . \n\n" +
+  "# continue query here; ?ontid is bound to the set of class IRIs of the OWL query \n\n"+
+	"}\n";
+
+    } else {
+      ta.value = "SELECT * WHERE {\n" +
+  " # enter query here \n\n" +
+  " # filter on ?x \n" +
+  " FILTER ( ?x IN ( OWL <?php echo $type; ?> <http://jagannath.pdn.cam.ac.uk/aber-owl/service/> <<?php echo $ontology; ?>> { <?php echo $query; ?> } ) ) . \n" +
+      " }\n";
+
+    }
+  }
+$(document).ready(function() { 
+    change('values');
+    $('#ta').autosize() ;
+} );
+  </script>
 </head>
 
 <body>
-    <form name="sparqowl" action="sparqowl.php" method="post">
-        <textarea name="query" cols="40" rows="5">Enter SPARQL query...</textarea>
+    <form name="sparqowl" action="sparqowl.php" method="POST">
+        <textarea id="ta" name="sparqlquery" cols="150" rows="5">
+</textarea>
+<a href="#" onclick="javascript:change('values');">Values form</a>
+<a href="#" onclick="javascript:change('filter');">Filter form</a>
 
         <br />
 
--- webinterface/sparqowl.php
+++ webinterface/sparqowl.php
@@ -4,15 +4,15 @@ require_once('sparqllib.php');
 
 if(php_sapi_name() === 'cli') {
     // do command line tings
-    $query = implode(' ', file('query.sparql'));
+    $sparqlquery = implode(' ', file('query.sparql'));
 } else {
-    $query = $_POST['query'];
-    $endpoint = $_POST['endpoint'];
+    $sparqlquery = $_POST['sparqlquery'];
+    $sparqlendpoint = $_POST['endpoint'];
 }
 
 // Load the query...
 
-$query = str_replace(array("\r\n", "\r", "\n", "\t", "  "), ' ', $query);
+//$sparqlquery = str_replace(array("\r\n", "\r", "\n", "\t", "  "), ' ', $sparqlquery);
 
 // Find and resolve OWL blocks
 // TODO: Resolve prefixes
@@ -20,32 +20,36 @@ $query = str_replace(array("\r\n", "\r", "\n", "\t", "  "), ' ', $query);
 /**
  * Match OWL ?id TYPEOFQUERY { Manchester Owl Syntax Query }
  *  0: Entire match
- *  1: Variable name
- *  2: Type of query (SUBCLASS etc)
- *  3: OWL endpoint URI
+ *  1: Type of query (SUBCLASS etc)
+ *  2: Endpoint URI
+ *  3: Ontology URI
  *  4: Manchester OWL Syntax query
  */
-preg_match_all("/OWL\s(\?\w+)?\s?(\w+)?\s*<(.+)>\s*{\s*(.+?)\s*?}/", $query, $owls, PREG_SET_ORDER);
+//preg_match_all("/OWL\s(\?\w+)?\s?(\w+)?\s*<(.+)>\s*{\s*(.+?)\s*?}/", $query, $owls, PREG_SET_ORDER);
+preg_match_all("/OWL\s+(\w+)\s*<(.+)>\s*<(.*)>\s*{\s*(.+?)\s*}/", $sparqlquery, $owls, PREG_SET_ORDER);
 foreach($owls as $owl) {
-    if(!$owl[1]) $owl[1] = '?id';
-    if(!$owl[2]) $owl[2] = ALL;
-    $result = owl_query($owl[3], $owl[4], $owl[2]);
-    if(!$result) {
-        print "OWL query failed. World ending.";
-        exit;
-    }
-    $values = parse_owl($result, $owl[1]);
+  $type = strtolower($owl[1]) ;
+  $endpoint = $owl[2] ;
+  $onturi = $owl[3] ;
+  $query = $owl[4] ;
 
-    // Replace OWL block with classes
-    $query = str_replace($owl[0], $values, $query);
+  $result = owl_query($endpoint, $onturi, $query, $type) ;
+  if(!$result) {
+    print "OWL query failed. World ending.";
+    exit;
+  }
+  $values = parse_owl($result, $owl[1]);
+
+  // Replace OWL block with classes
+  $sparqlquery = str_replace($owl[0], $values, $sparqlquery);
 }
 
-print 'Resolved Query: ' . $query . '<br /><br />';
+print 'Resolved Query: ' . $sparqlquery . '<br /><br />';
 
 // Run the SPARQL query
 
-$db = sparql_connect($endpoint);
-$result = sparql_query(htmlspecialchars_decode($query));
+$db = sparql_connect($sparqlendpoint);
+$result = sparql_query(htmlspecialchars_decode($sparqlquery));
 if(!$result) {
     print sparql_errno() . ': ' . sparql_error() . '<br />';
     exit;
@@ -63,10 +67,11 @@ while($row = sparql_fetch_array($result)) {
 }
 
 // Perform a remote OWL query
-function owl_query($endpoint, $query, $type) {
+function owl_query($endpoint, $onturi, $query, $type) {
     $url = $endpoint . '?query=' . urlencode($query) . 
-        '&type=' . strtolower($type);
-    print $url;
+        '&type=' . strtolower($type) .
+      '&ontology=' . $onturi;
+    //print $url;
     $request = curl_init($url);
     curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($request, CURLOPT_HTTPHEADER, array(
@@ -90,14 +95,13 @@ function owl_query($endpoint, $query, $type) {
 // OWL ?id
 function parse_owl($owl, $idname) {
     $data = json_decode($owl, true);
-    $values = 'VALUES ' . $idname . ' { ';
+    $values = "" ;
     foreach($data as $object) {
         if($object['iri']['remainder'] != 'Thing' && $object['iri']['remainder'] != 'Nothing') {
-		$uri = $object['iri']['prefix'] . $object['iri']['remainder'];
-		$values .= ' &lt;' . $uri . '&gt; ';
+		$uri = $object['classURI'];
+		$values .= ' <' . $uri . '> ';
         }
     }
-    $values .= '} .';
 
     return $values;
 }
