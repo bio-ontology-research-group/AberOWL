d55fdb2f5b8760180dea079e9d409cafca594c43
reality <tinmachin3@gmail.com>
2015-02-04 10:50:48 +0300

    fix imports

--- groovy/RemoteOntologyManager.groovy
+++ groovy/RemoteOntologyManager.groovy
@@ -49,7 +49,7 @@ class RemoteOntologyManager {
           SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
           def lastSubDate = dateFormat.parse(submissions[0].released).toTimestamp().getTime() / 1000;
           
-          if(lastSubDate > exOnt.lastSubDate) { 
+          if(lastSubDate > exOnt.lastSubDate) {
             exOnt.addNewSubmission([
               'released': lastSubDate,
               'download': ont.links.download
@@ -70,5 +70,4 @@ class RemoteOntologyManager {
     RemoteOntologyManager r = new RemoteOntologyManager()
     r.updateOntologies();
   }
-
 }
