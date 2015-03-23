4976da05eb3602af2f97ac0b270aeb04a3b96184
Luke Slater <tinmachin3@gmail.com>
2015-03-23 09:11:09 +0000

    Fix api functions

--- groovy/api/getStats.groovy
+++ groovy/api/getStats.groovy
@@ -9,7 +9,6 @@ import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.io.*;
 
-
 if(!application) {
   application = request.getApplication(true)
 }
@@ -17,30 +16,34 @@ if(!application) {
 def ontology = request.getParameter('ontology')
 def rManager = application.rManager
 
+if(ontology) {
+	def ont = rManager.ontologies.get(ontology)
+	def stats = [
+	  'rBoxAxiomCount': 0,
+	  'tBoxAxiomCount': 0,
+	  'totalAxiomCount': 0,
+	  'logicalAxiomCount': 0,
+	  'complexity': 0,
+	  'classCount': ont.getClassesInSignature(true).size(),
+	  'consistent': rManager.queryEngines.get(ontology) != null
+	]
 
-def ont = rManager.ontologies.get(ontology)
-def stats = [
-  'rBoxAxiomCount': 0,
-  'tBoxAxiomCount': 0,
-  'totalAxiomCount': 0,
-  'logicalAxiomCount': 0,
-  'complexity': 0,
-  'classCount': ont.getClassesInSignature(true).size()
-]
+	AxiomType.TBoxAxiomTypes.each { ont.getAxioms(it, true).each { 
+	  stats.totalAxiomCount += 1
+	  stats.tBoxAxiomCount += 1
+	} }
+	AxiomType.RBoxAxiomTypes.each { ont.getAxioms(it, true).each { 
+	  stats.totalAxiomCount += 1 
+	  stats.rBoxAxiomCount += 1 
+	} }
 
-AxiomType.TBoxAxiomTypes.each { ont.getAxioms(it, true).each { 
-  stats.totalAxiomCount += 1
-  stats.tBoxAxiomCount += 1
-} }
-AxiomType.RBoxAxiomTypes.each { ont.getAxioms(it, true).each { 
-  stats.totalAxiomCount += 1 
-  stats.rBoxAxiomCount += 1 
-} }
-
-stats.complexity = stats.totalAxiomCount / stats.classCount
-stats.logicalAxiomCount = ont.getLogicalAxiomCount()
-
+	stats.complexity = stats.totalAxiomCount / stats.classCount
+	stats.logicalAxiomCount = ont.getLogicalAxiomCount()
 response.contentType = 'application/json'
 print JSONValue.toJSONString(stats)
+} else {
+	stats = rManager.getStats()
+response.contentType = 'application/json'
+print JSONValue.toJSONString(stats)
+}
 
-print rManager.queryEngines.size()
--- groovy/api/listOntologies.groovy
+++ groovy/api/listOntologies.groovy
@@ -3,5 +3,6 @@ import org.json.simple.JSONValue;
 
 def rManager = application.rManager
 response.contentType = 'application/json'
-def keys = rManager.queryEngines.keySet()
+def keys = rManager.queryEngines.keySet().toArray().sort(false)
+
 print new JsonBuilder(keys).toString()
