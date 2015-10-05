7e0c86165cd7e38d199ad114044cfa30a0ef1b9c
Robert Hoehndorf <robert.hoehndorf@kaust.edu.sa>
2015-10-06 00:34:54 +0300

    added full path to findRoot

--- api/findRoot.groovy
+++ api/findRoot.groovy
@@ -31,7 +31,7 @@ if(ontology && query) {
     if (fOut.size() > 0) { // class found
       ArrayList out = null
       resultMap['classes'] = fOut
-      
+      resultMap['children'] = rManager.runQuery(query, 'subclass', ontology, -1, true, false).toArray()
       def thingFound = false
       while(thingFound == false) {
 	out = rManager.runQuery(query, 'superclass', ontology, -1, true, false).toArray()
@@ -39,16 +39,18 @@ if(ontology && query) {
 	//	if(!out || (out && out[0].owlClass == '<http://www.w3.org/2002/07/owl#Thing>')) {
 	if(!out || (out && out.findAll{it.owlClass == '<http://www.w3.org/2002/07/owl#Thing>'}.size()>0)) {
 	  thingFound = true
-	  break
 	} else {
+	  def oldquery = query
 	  query = out[0].owlClass
+	  def nOut = rManager.runQuery(query, 'subclass', ontology, -1, true, false).findAll { it.owlClass != oldquery }.sort { it.label[0] }
+	  resultMap.classes += nOut
+	  out[0]["children"] = resultMap
+
+	  resultMap = [ 'classes': out ]
 	}
-	
-	out[0].children = resultMap
-	resultMap = [ 'classes': out ]
       }
       
-      def nOut = rManager.runQuery('<http://www.w3.org/2002/07/owl#Thing>', 'subclasses', ontology, -1, true, false).findAll { it.owlClass != query }.sort {it.label[0] }
+      def nOut = rManager.runQuery('<http://www.w3.org/2002/07/owl#Thing>', 'subclass', ontology, -1, true, false).findAll { it.owlClass != query }.sort { it.label[0] }
       
       // THIS WORKS ON GOOD GROOVY VERSIONS > 2.0
       resultMap.classes += nOut
@@ -56,7 +58,7 @@ if(ontology && query) {
       resultMap['chosen'] = fOut[0]
       
     } else {
-      resultMap['classes'] = rManager.runQuery('<http://www.w3.org/2002/07/owl#Thing>', 'subclasses', ontology, -1, true, false).sort { it.label[0] }
+      resultMap['classes'] = rManager.runQuery('<http://www.w3.org/2002/07/owl#Thing>', 'subclass', ontology, -1, true, false).sort { it.label[0] }
     }
     response.contentType = 'application/json'
     print new JsonBuilder(resultMap).toString()
