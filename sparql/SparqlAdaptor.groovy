2cf36999a69fe5b7caa1eb341dd6f818c9f3975b
MiguelAngelRG <mangelrg21@gmail.com>
2016-04-04 11:02:34 +0300

    Developing the Sparql Adaptor to connect aberowl with virtuoso repository

--- sparql/SparqlAdaptor.groovy
+++ sparql/SparqlAdaptor.groovy
@@ -10,22 +10,41 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 import org.semanticweb.owlapi.io.FileDocumentSource;
 import org.apache.jena.rdf.model.*;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.util.iterator.*;
 import org.apache.jena.util.*;
+import org.apache.jena.riot.web.LangTag;
 
 public class SparqlAdaptor {
 
+    /**
+     * It represents the url where the virtuoso server is located
+     */
     private String virtuosoURL;//URL where is located the virtuoso server.
+    /**
+     * The user name in the virtuoso server
+     */
     private String virtuosoUser;//User name
+    /**
+     * The password needed to access to the repository
+     */
     private String virtuosoPasswd;//Password
+    /**
+     * The based name that will be used to insert graphs in the virtuoso repository.
+     */
     private String graphBasedName="http://aber-owl.net/";
 
+    /**
+     * Constructor of the adaptor.
+     */
     public SparqlAdaptor(){
         this.initialize()
     }
 
-
-    public void initialize(){
+    /**
+     * It initializes the properties needed to connect to the repository.
+     */
+    private void initialize(){
         try{
             String pathFile = (System.getProperty("user.dir")+File.separator+"resources"+File.separator+"virtuoso_config.properties");
             Properties properties = new Properties();
@@ -38,7 +57,10 @@ public class SparqlAdaptor {
                 System.out.println("ERROR: It was not possible to access to the property file");
         }
     }
-
+    /**
+     * It performs the connection to virtuoso repository.
+     * @return Connection to the repository..
+     */
     private Connection virtuosoConnection() {
         Connection connection=null;
         try{
@@ -51,46 +73,135 @@ public class SparqlAdaptor {
         return connection;
     }
 
+    /**
+     * It escapes SPARQL language. The function has been implemented according to the guidelines provided in W3C
+     * standard SPARQL Query Language for RDF.
+     * @param literal The literal that will be checked. If this literal contain any reserverd word, it will be then
+     * escaped using \\.
+     * @return Literal checked..
+     */
+    public String escapeSparql(String literal) {
+        //To avoid problems in sparql.
+        if (literal.contains("\\\"")) {
+            literal = literal.replace("\\\"", "\"")
+        }
+        if (literal.contains("\\'")) {
+            literal = literal.replace("\\'", "'")
+        }
+        literal = literal.replace("\\", "\\\\");
+        literal = literal.replace("\"", "\\\"");
+        literal = literal.replace("'", "\\'");
+        literal = literal.replace("\t", "\\t");
+        literal = literal.replace("\n", "\\n");
+        literal = literal.replace("\r", "\\r");
+        literal = literal.replace("\b", "\\b");
+        literal = literal.replace("\f", "\\f");
+        return (literal)
+    }
+
+    /**
+     * It inserts graph in virtuoso.
+     * @param graphName The name used to insert the graph.
+     * @param graphModel The Jena model that contains the ontology, which will be updated in the repository.
+     */
     public void insertGraph(String graphName, Model graphModel) {
         try{
             Connection kbConn = virtuosoConnection();
             Statement stmt = kbConn.createStatement();
             try {
-                String deleteTriples = "SPARQL CLEAR GRAPH <"+graphBasedName+graphName+">"
+                String deleteTriples = "SPARQL CLEAR GRAPH <"+graphBasedName+graphName+">";
                 stmt.execute(deleteTriples);
                 String dropGraph = "SPARQL DROP GRAPH <"+graphBasedName+graphName+">";
                 stmt.execute(dropGraph);
             }catch(Exception e) {
-                System.out.println("Warning: the graph it was not deleted because it was not created before")
+                System.out.println("Warning: the graph it was not deleted because it was not created before");
             }
             String createGraph = "SPARQL CREATE GRAPH <"+graphBasedName+graphName+">";
             stmt.execute(createGraph);
             //String loadGraph = "SPARQL LOAD <file:/../../../.."+graphFile.getAbsolutePath()+"> INTO <http://aber-owl.net/"+name+">";
-            //stmt.execute(loadGraph);
+            // stmt.execute(loadGraph);
             StmtIterator stmIt = graphModel.listStatements();
-            String sentence;
             org.apache.jena.rdf.model.Statement statement = null;
+            String sentence;
             while(stmIt.hasNext()){
                 statement = stmIt.nextStatement();
                 if(statement.getObject().isLiteral()) {
-                    sentence = "SPARQL INSERT INTO GRAPH <"+graphBasedName+graphName+"> { <"+statement.getSubject().getURI()+"> <"+statement.getPredicate().toString()+"> " + "<"+statement.getObject().asLiteral().getString()+"> }";
+                    Literal literal =  (Literal)statement.getObject();
+                    if(!literal.getLanguage().isEmpty()) {
+                        //Canonicalize with the rules of RFC 4646
+                        if(((!literal.getLanguage().isEmpty()))&&(LangTag.parse(literal.getLanguage())!=null)) {
+                            sentence = "SPARQL INSERT INTO GRAPH <" + graphBasedName + graphName + "> { " +
+                                    "<" + statement.getSubject().getURI() + "> " +
+                                    "<" + statement.getPredicate().toString() + "> " +
+                                    "\"" + escapeSparql(literal.getValue().toString()) + "\"@" + literal.getLanguage() + " }";
+                        }else{//if the language has been defined incorrectly, then we insert the literal without language
+                            sentence = "SPARQL INSERT INTO GRAPH <" + graphBasedName + graphName + "> { " +
+                                    "<" + statement.getSubject().getURI() + "> " +
+                                    "<" + statement.getPredicate().toString() + "> " +
+                                    "\""+ escapeSparql(literal.getValue().toString())+"\" }";
+                        }
+                    }
                 }else if(statement.getObject().isResource()){
-                    sentence = "SPARQL INSERT INTO GRAPH <"+graphBasedName+graphName+"> { <"+statement.getSubject().getURI()+"> <"+statement.getPredicate().toString()+"> "+ "<"+statement.getObject().asResource().getURI()+"> }";
+                    Resource resource =  (Resource)statement.getObject();
+                    sentence = "SPARQL INSERT INTO GRAPH <"+graphBasedName+graphName+"> { " +
+                            "<"+statement.getSubject().getURI()+"> " +
+                            "<"+statement.getPredicate().toString()+"> " +
+                            "<"+resource.getURI()+"> }";
                 }
-                stmt.execute(sentence)
+                stmt.executeUpdate(sentence)
             }
             stmt.close();
             kbConn.close()
             System.out.println("Successfully loadded "+graphName+" in the repository");
         }catch(Exception e) {
             e.printStackTrace()
-            System.out.println("Error:It was not possible to access to the RDF repository.");
         }
-
     }
 
-    protected List<String> querySparql(String graphName, String concept, List<String> properties) {
-        ArrayList<String> children = new ArrayList<String>();
+    /**
+     * Given an ontology name, it returns its list of properties (predicates) stored in virtuoso.
+     * @param ontologyName The name of the graph from where the properties (predicates) will be queried.
+     * @return Map of properties.The format used to store this information is:
+     * name: it represents the name of the property
+     * uri: it represents the uri of the property
+     */
+    protected List<HashMap<String,String>> getProperties(String ontologyName){
+        ArrayList<HashMap<String,String>> properties = new ArrayList<HashMap<String,String>>()
+        try{
+            Connection kbConn = virtuosoConnection();
+            Statement stmt = kbConn.createStatement();
+            sentence = "SPARQL SELECT DISTINCT ?p FROM <"+graphBasedName+ontologyName+"> WHERE { ?s ?p ?o}"
+            ResultSet resultSet = stmt.executeQuery(sentence);
+            String property;
+            while(resultSet.next()){
+                property = resultSet.getString(1);
+                if(property!=null) {
+                    HashMap<String,String> hashProp = new HashMap<String,String>();
+                    String name = object.substring(property.lastIndexOf("/")+1,property.length())
+                    name = name.replace("#","");
+                    hashProp.put("name",name);
+                    hashProp.put("uri",property);
+                    properties.add(name,property);
+                }
+            }
+            stmt.close();
+            kbConn.close();
+        }catch(Exception e){
+            System.out.println("ERROR: It was not possible to access to the RDF repository.")
+        }
+
+        return(children);
+    }
+    /**
+     * Given an ontology name, literal's label and list of propertiesl, it returns all resources related according
+     * to the properties given.
+     * @param graphName The name of the graphs.
+     * @param label The label of the literal
+     * @param properties The list of properties
+     * @return List of labels, which are related to the given label.
+     */
+    protected List<String> querySparql(String graphName, String label, List<String> properties) {
+        HashMap<String,HashMap<String,String>> children = new HashMap<String,HashMap<String,String>>();
         try{
             Connection kbConn = virtuosoConnection();
             Statement stmt = kbConn.createStatement();
@@ -99,7 +210,7 @@ public class SparqlAdaptor {
             String property;
             while(itProperties.hasNext()){
                 property = itProperties.next();
-                sentence = "SPARQL SELECT ?s FROM <"+graphBasedName+graphName+"> WHERE { ?s <"+property+"> <"+concept+">}"
+                sentence = "SPARQL SELECT ?s FROM <"+graphBasedName+graphName+"> WHERE { ?s <"+property+"> <"+escapeSparql(label)+">}"
                 ResultSet resultSet = stmt.executeQuery(sentence);
                 String object;
                 while(resultSet.next()){
@@ -118,8 +229,15 @@ public class SparqlAdaptor {
         return(children);
     }
 
+    /**
+     * It converts an ontology into RDF graph
+     * @param name The ontology's name that is used to store it in the repository.
+     * @param timestamp The timestamp of this ontology to differentiate it to others versions.
+     * @param owlFile The ontology file.
+     */
     public void convertOntology(String name, String timestamp, FileDocumentSource owlFile){
         try {
+            println("Converting ontology:"+name+" into RDF graph...")
             String[] reasoners=['ELK'];
             String[] formats=['RDFXML']
             boolean eqClasses = true;
@@ -139,14 +257,15 @@ public class SparqlAdaptor {
                                     " -r " + reasoner +
                                     " -f " + format +
                                     " -eq " + eqClasses.toString() +
-                                    " -op [*]";
+                                    " -op [*]" +
+                                    " -nt 4";
 
                             def process = command.execute();
                             def outputStream = new StringBuffer();
                             process.consumeProcessErrorStream(outputStream);
                             println process.text
                             println outputStream.toString()
-                            
+
                             Model graphModel = ModelFactory.createDefaultModel();
                             InputStream input = FileManager.get().open(graphFile.getAbsolutePath())
                             graphModel.read(input,"")
